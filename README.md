1a3cdd22c971ca13ce06a48eaaa00d2a6602b5c1
Gabriel <gvictorinocardoso@gmail.com>
2024-04-03 19:22:00 +0200

    Fixed bug in measurement coordinates, updating readme

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ This repository can be installed as a python package by running
 `pip install -I .` on the root folder of this git.
 
 You can try this algorithm either with the available scripts in this project or in the benchopts, with comparison to other algorithms at https://github.com/gabrielvc/benchopts_inverse_problem_diffusion_prior/tree/master
+
+
+## Results using the hugging face models on CelebA
+
+
+The following table was produced by running the `scripts/hugging_faces_models.py` with the configurations described below.
+The configuration for MCG DIFF is defined in `scripts/configs/mcg_diff/default.yaml`. Running this script took 3 minutes to generate each image, but
+this can be made faster by using parallelization.
+
+| Original image                       | Observation | Sample | Sample | Changes to `config.yaml`         |
+|--------------------------------------| ---- | ---- | ---- |----------------------------------|
+| ![image](images/deblur_y_or.png)     | ![image](images/deblur_obs.png)| ![image](images/deblur_sample_1.png)| ![image](images/deblur_sample_2.png)| `seed = 32`, `task = deblur_2d`  |
+| ![image](images/inpainting_y_or.png) | ![image](images/inpainting_obs.png)| ![image](images/inpainting_sample_1.png)| ![image](images/inpainting_sample_2.png)| `seed = 15`, `task = inpainting` |
+| ![image](images/sr_y_or.png) | ![image](images/sr_obs.png)| ![image](images/sr_sample_1.png)| ![image](images/sr_sample_2.png)| `seed = 10`, `task = sr`         |
--- mcg_diff/particle_filter.py
+++ mcg_diff/particle_filter.py
@@ -90,19 +90,21 @@ def mcg_diff(
                                                        t_prev=t_prev,
                                                        eta=eta,
                                                        n_samples_per_gpu=n_samples_per_gpu_inference)
-        active_coordinates = coordinates_in_state[t_prev >= taus]
-        inactive_coordinates = torch.cat((coordinates_in_state[t_prev < taus], always_free_coordinates), dim=0)
-        previously_active_coordinates = coordinates_in_state[t >= taus]
+        active_coordinates_in_obs = torch.where(t_prev >= taus)[0]
+        previously_active_coordinates_in_obs = torch.where(t >= taus)[0]
+        active_coordinates_in_x = coordinates_in_state[active_coordinates_in_obs]
+        inactive_coordinates_in_x = torch.cat((coordinates_in_state[t_prev < taus], always_free_coordinates), dim=0)
+        previously_active_coordinates_in_x = coordinates_in_state[previously_active_coordinates_in_obs]
 
         #Calculation of weights
         previous_log_likelihood = gauss_loglik(
-            x=particles[:, previously_active_coordinates],
-            mean=rescaled_observations[previously_active_coordinates] * (alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates]])**.5,
-            diag_std=(1 - (1 - gaussian_var) * (alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates]]))**.5)
+            x=particles[:, previously_active_coordinates_in_x],
+            mean=rescaled_observations[previously_active_coordinates_in_obs] * (alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates_in_obs]])**.5,
+            diag_std=(1 - (1 - gaussian_var) * (alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates_in_obs]]))**.5)
         log_integration_constant = gauss_loglik(
-            x=predicted_mean[:, active_coordinates],
-            mean=rescaled_observations[active_coordinates] * ((alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates]])**.5),
-            diag_std=(predicted_noise ** 2 + 1 - (1 - gaussian_var)*(alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates]]))**.5
+            x=predicted_mean[:, active_coordinates_in_x],
+            mean=rescaled_observations[active_coordinates_in_obs] * ((alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates_in_obs]])**.5),
+            diag_std=(predicted_noise ** 2 + 1 - (1 - gaussian_var)*(alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates_in_obs]]))**.5
         )
         log_weights = log_integration_constant - previous_log_likelihood
 
@@ -110,22 +112,23 @@ def mcg_diff(
         ancestors = Categorical(logits=log_weights, validate_args=False).sample((n_particles,))
         #Update
         z = torch.randn_like(particles)
-        Kprev = (predicted_noise**2 / (predicted_noise**2 + 1 - (1 - gaussian_var)*(alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates]])).clip(1e-10, 1e10))
+        Kprev = (predicted_noise**2 / (predicted_noise**2 + 1 - (1 - gaussian_var)*(alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates_in_obs]])).clip(1e-10, 1e10))
         new_particles = particles.clone()
-        new_particles[:, inactive_coordinates] = z[:, inactive_coordinates] * predicted_noise + predicted_mean[ancestors][:, inactive_coordinates]
-        new_particles[:, active_coordinates] = Kprev * rescaled_observations[active_coordinates][None,:] * ((alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates]])**.5) + \
-                                   (1 - Kprev)*predicted_mean[ancestors][:, active_coordinates] + \
-                                   ((1 - (1 - gaussian_var)*(alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates]]))*Kprev)**.5 * z[:, active_coordinates]
+        new_particles[:, inactive_coordinates_in_x] = z[:, inactive_coordinates_in_x] * predicted_noise + predicted_mean[ancestors][:, inactive_coordinates_in_x]
+        new_particles[:, active_coordinates_in_x] = Kprev * rescaled_observations[active_coordinates_in_obs][None,:] * ((alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates_in_obs]])**.5) + \
+                                   (1 - Kprev)*predicted_mean[ancestors][:, active_coordinates_in_x] + \
+                                   ((1 - (1 - gaussian_var)*(alphas_cumprod[t_prev] / alphas_cumprod[taus[active_coordinates_in_obs]]))*Kprev)**.5 * z[:, active_coordinates_in_x]
 
         particles = new_particles
 
     t = filtering_timesteps[0]
-    previously_active_coordinates = coordinates_in_state[t >= taus]
+    previously_active_coordinates_in_obs = torch.where(t >= taus)[0]
+    previously_active_coordinates_in_x = coordinates_in_state[previously_active_coordinates_in_obs]
     previous_log_likelihood = gauss_loglik(
-        x=particles[:, previously_active_coordinates],
-        mean=rescaled_observations[previously_active_coordinates] * (
-                alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates]]) ** .5,
-        diag_std=(1 - (1 - gaussian_var) * (alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates]]))**.5)
+        x=particles[:, previously_active_coordinates_in_x],
+        mean=rescaled_observations[previously_active_coordinates_in_obs] * (
+                alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates_in_obs]]) ** .5,
+        diag_std=(1 - (1 - gaussian_var) * (alphas_cumprod[t] / alphas_cumprod[taus[previously_active_coordinates_in_obs]]))**.5)
     if len(propagation_timesteps) > 1:
         # If Tau_1 > 0 we still have to propagate using the diffusion between tau_1 and 0
         pbar = enumerate(zip(propagation_timesteps.tolist()[1:][::-1],
